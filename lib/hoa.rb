1e3658ac5c69d088a9e4150e4021332f576c5963
Connor Blakeney <connorblakeney@yahoo.com>
2020-02-19 22:04:21 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,10 +9,12 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
 {
-  :chipmunks => ["Alvin", "Simon", "Theodore", "Dave"],
+  :chipmunks => ["Alvin", "Simon", "Theodore"],
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
 }
 end
+
+:chipmunks << "Dave"
 
 
